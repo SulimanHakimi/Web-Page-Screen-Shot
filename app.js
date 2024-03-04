81616e900752753b1c32ad5b843e04b7d8a624d4
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-03 19:33:38 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("screen");
+btn.addEventListener("click", () => {
+  html2canvas(document.body).then((canvas) => {
+    const url = canvas.toDataURL("image/png");
+    const a = document.createElement("a");
+    a.setAttribute("download", "imageName.png");
+    a.setAttribute("href", url);
+    a.click();
+  });
+});
